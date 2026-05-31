5e41fa22e4c4485f0a93bbb53c0642e7dd6c5e42
Arihant Bansal <f20240629@goa.bits-pilani.ac.in>
2026-05-31 08:37:15 +0000

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(helmet());
-app.use(cors({ origin: process.env.NODE_ENV === 'production' ? process.env.FRONTEND_URL : 'http://localhost:3000' }));
+app.use(cors({ origin: '*' }));
 app.use(express.json({ limit: '10kb' }));
 app.use(morgan('dev'));
 
